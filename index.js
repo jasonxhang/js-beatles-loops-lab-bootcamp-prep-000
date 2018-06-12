6fd1ee70371ee131c032c953974020ec9fbdf65c
Jason Hang <jasonxhang@gmail.com>
2018-06-12 19:11:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay (arrayMusicians, arrayInstruments) {
+  var newArray = [];
+  for (var i = 0; i < arrayMusicians.length; i++) {
+    newArray.push(`${arrayMusicians[i]} plays ${arrayInstruments[i]}`);
+  }
+  return newArray;
+}
+
+function johnLennonFacts (facts) {
+  var newFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    newFacts.push(`${facts[i]}!!!`)
+  } i++;
+  return newFacts;
+}
